d24b395fb2ed413ec0981651459997b6c603758b
Fernando Sotelo <warsha975@gmail.com>
2025-09-03 01:02:26 -0500

    practicando 1
    xd

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+export default function App() {
+  return(
+
+    <>
+      <h1>App</h1>
+      <Button variant="contained">Mi primer boton</Button>
+    </>
+  )
+
+}
